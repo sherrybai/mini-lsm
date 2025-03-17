e91957aee5146133ca79a703c1982dc63d564203
Sherry Bai <sherrybai01@gmail.com>
2025-03-17 14:39:22 -0400

    ordering for keys

--- src/kv/timestamped_key.rs
+++ src/kv/timestamped_key.rs
@@ -1,6 +1,6 @@
 use bytes::Bytes;
 
-#[derive(Eq, Ord, PartialEq, PartialOrd, Clone, Debug)]
+#[derive(Eq, PartialEq, Clone, Debug)]
 pub struct TimestampedKey {
     key: Bytes,
     timestamp_ms: usize,
@@ -10,7 +10,39 @@ impl TimestampedKey {
     pub fn new(key: Bytes) -> Self {
         TimestampedKey {
             key,
-            timestamp_ms: 0,  // TODO: set timestamp later
+            timestamp_ms: 0, // TODO: set timestamp later
         }
+    }
+}
+
+impl Ord for TimestampedKey {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        // first compare keys lexicographically
+        // if two keys are equal, then latest timestamp is smaller
+        self.key
+            .cmp(&other.key)
+            .then(other.timestamp_ms.cmp(&self.timestamp_ms))
+    }
+}
+
+impl PartialOrd for TimestampedKey {
+    fn partial_cmp(&self, other: &TimestampedKey) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(&other))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::TimestampedKey;
+
+    #[test]
+    fn test_ord() {
+        let tk1 = TimestampedKey{key: "k1".into(), timestamp_ms: 100};
+        let tk2 = TimestampedKey{key: "k1".into(), timestamp_ms: 0};
+        let tk3 = TimestampedKey{key: "k2".into(), timestamp_ms: 100};
+
+        assert!(tk1 < tk2);
+        assert!(tk1 < tk3);
+        assert!(tk2 < tk3);
     }
 }
