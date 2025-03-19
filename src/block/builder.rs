bd85c3525770fcc8be095149875648b33f0af423
Sherry Bai <sherrybai01@gmail.com>
2025-03-19 13:20:03 -0400

    get block size in builder

--- src/block/builder.rs
+++ src/block/builder.rs
@@ -24,13 +24,13 @@ impl BlockBuilder {
         let value_len_bytes = u16::try_from(kv_pair.value.len())?.to_be_bytes();
         let kv_as_bytes: Vec<u8> = vec![
             key_len_bytes.to_vec(),
-            kv_pair.key.get_key().to_vec(), 
+            kv_pair.key.get_key().to_vec(),
             value_len_bytes.to_vec(),
-            kv_pair.value.to_vec()
+            kv_pair.value.to_vec(),
         ]
-            .into_iter()
-            .flatten()
-            .collect();
+        .into_iter()
+        .flatten()
+        .collect();
         self.offsets.push(self.current_offset);
         self.current_offset += u16::try_from(kv_as_bytes.len())?;
         self.data.extend(kv_as_bytes);
@@ -39,11 +39,13 @@ impl BlockBuilder {
     }
 
     pub fn build(self) -> Block {
-        Block::new(
-            self.data,
-            self.offsets,
-            self.current_offset
-        )
+        Block::new(self.data, self.offsets, self.current_offset)
+    }
+
+    pub fn get_block_size(&self) -> usize {
+        self.data.len() // data in bytes
+        + 2 * self.offsets.len() // each offset is 2 bytes
+        + 2 // end of data offset is 2 bytes
     }
 }
 
@@ -56,24 +58,30 @@ mod tests {
     #[test]
     fn test_blockbuilder_build() {
         let mut block_builder = BlockBuilder::new();
-        assert!(block_builder.add(KeyValuePair { key: TimestampedKey::new("k1".as_bytes().into()), value: "v1".as_bytes().into() }).is_ok());
-        assert!(block_builder.add(KeyValuePair { key: TimestampedKey::new("k2".as_bytes().into()), value: "v2".as_bytes().into() }).is_ok());
+        assert!(block_builder
+            .add(KeyValuePair {
+                key: TimestampedKey::new("k1".as_bytes().into()),
+                value: "v1".as_bytes().into()
+            })
+            .is_ok());
+        assert!(block_builder
+            .add(KeyValuePair {
+                key: TimestampedKey::new("k2".as_bytes().into()),
+                value: "v2".as_bytes().into()
+            })
+            .is_ok());
 
         let actual = block_builder.build();
 
-        let mut expected_data = vec![0,2];
+        let mut expected_data = vec![0, 2];
         expected_data.extend("k1".as_bytes());
-        expected_data.extend(vec![0,2]);
+        expected_data.extend(vec![0, 2]);
         expected_data.extend("v1".as_bytes());
-        expected_data.extend(vec![0,2]);
+        expected_data.extend(vec![0, 2]);
         expected_data.extend("k2".as_bytes());
-        expected_data.extend(vec![0,2]);
+        expected_data.extend(vec![0, 2]);
         expected_data.extend("v2".as_bytes());
-        let expected = Block::new(
-            expected_data, 
-            vec![0, 8], 
-            16
-        );
+        let expected = Block::new(expected_data, vec![0, 8], 16);
         assert_eq!(actual, expected);
     }
 }
